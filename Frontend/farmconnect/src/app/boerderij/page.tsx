b91c3d29472b8ce20952935a578204625105cbcf
Dave <77780263+BitsAndDroids@users.noreply.github.com>
2023-05-03 14:15:17 +0200

    Added propper association relationships

---
+++ Frontend/farmconnect/src/app/boerderij/page.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const BoerderijPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Boerderij</h1>
+        </div>
+    )
+}
+
+export default BoerderijPage;
