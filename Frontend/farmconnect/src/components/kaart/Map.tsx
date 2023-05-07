cd07fdfc3e2717411f96d931851128f3b6674d25
jdjan <20067879@student.hhs.nl>
2023-05-07 12:02:50 +0200

    Basic Map Component + Kaart page

---
+++ Frontend/farmconnect/src/components/kaart/Map.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import styles from './Map.module.css';
+
+type MapProps = {
+  farms: { id: number; latitude: number; longitude: number; name: string }[];
+};
+
+const MapEffect: React.FC = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 100);
+  }, [map]);
+
+  return null;
+};
+
+const Map: React.FC<MapProps> = ({ farms }) => {
+  const center = [52.3783, 4.9009];
+
+  return (
+    <div className={styles.mapWrapper}>
+      <MapContainer
+        center={center}
+        zoom={8}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <MapEffect />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {farms.map((farm) => (
+          <Marker key={farm.id} position={[farm.latitude, farm.longitude]}>
+            <Popup>{farm.name}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
