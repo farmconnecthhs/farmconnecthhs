58c1a0b2306c86d608a083a8214a25b9edcf78c9
jdjan <20067879@student.hhs.nl>
2023-05-10 20:09:58 +0200

    Added couple extra farms for a more poplated map.

--- Frontend/farmconnect/src/components/kaart/MapComponent.tsx
+++ Frontend/farmconnect/src/components/kaart/MapComponent.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useState, useEffect } from 'react';
 
 import Map from '@/components/kaart/Map';
@@ -8,19 +7,61 @@ const farms = [
     id: 1,
     latitude: 52.3783,
     longitude: 4.9009,
-    name: 'Farm 1',
+    name: 'Groene Weiden Boerderij',
   },
   {
     id: 2,
     latitude: 52.4783,
     longitude: 4.8009,
-    name: 'Farm 2',
+    name: 'Zonnige Velden Hoeve',
   },
   {
     id: 3,
     latitude: 52.2783,
     longitude: 4.7009,
-    name: 'Farm 3',
+    name: 'Bloeiende Akkers Landgoed',
+  },
+  {
+    id: 4,
+    latitude: 51.9222,
+    longitude: 4.4805,
+    name: 'Waterkant Boerderij',
+  },
+  {
+    id: 5,
+    latitude: 52.0909,
+    longitude: 5.1057,
+    name: 'Windmolen Gaard',
+  },
+  {
+    id: 6,
+    latitude: 52.3729,
+    longitude: 4.6561,
+    name: 'Kustlijn Hoeve',
+  },
+  {
+    id: 7,
+    latitude: 53.2223,
+    longitude: 6.5674,
+    name: 'Groninger Velden Landgoed',
+  },
+  {
+    id: 8,
+    latitude: 51.4416,
+    longitude: 5.4697,
+    name: 'Brabantse Heide Boerderij',
+  },
+  {
+    id: 9,
+    latitude: 52.5063,
+    longitude: 6.0819,
+    name: 'Twentse Akkers Gaard',
+  },
+  {
+    id: 10,
+    latitude: 51.9851,
+    longitude: 5.8987,
+    name: 'Gelderse Landgoed',
   },
 ];
 
