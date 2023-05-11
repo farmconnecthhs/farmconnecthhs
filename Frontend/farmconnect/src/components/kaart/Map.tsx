0e4828756414fff05421e70bcf04bc5586bd0432
Dave <77780263+BitsAndDroids@users.noreply.github.com>
2023-05-11 13:56:04 +0200

    minor map change

--- Frontend/farmconnect/src/components/kaart/Map.tsx
+++ Frontend/farmconnect/src/components/kaart/Map.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
@@ -11,19 +11,20 @@ interface MapProps {
   farms: { id: number; latitude: number; longitude: number; name: string }[];
 }
 
-const MapController: React.FunctionComponent = () => {
-  const map = useMap();
-
-  useEffect(() => {
-    setTimeout(() => {
-      map.invalidateSize();
-    }, 100);
-  }, [map]);
-
-  return null;
-};
 const Map: React.FunctionComponent<MapProps> = ({ farms }) => {
-  const center: LatLngExpression = [52.3783, 4.9009];
+  const [center, setCenter] = React.useState<LatLngExpression>([
+    52.3783, 4.9009,
+  ]);
+
+  const mapRef: any = React.useRef(null);
+
+  const handleMapMove = useCallback(() => {
+    const map = mapRef.current;
+    if (map != null) {
+      const newCenter = map.leafletElement.getCenter();
+      setCenter([newCenter.lat, newCenter.lng]);
+    }
+  }, []);
 
   return (
     <div className={styles.mapContainer}>
@@ -31,12 +32,12 @@ const Map: React.FunctionComponent<MapProps> = ({ farms }) => {
         center={center}
         zoom={8}
         style={{ height: '100%', width: '100%' }}
+        ref={mapRef}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <MapController />
         {farms.map((farm) => (
           <Marker key={farm.id} position={[farm.latitude, farm.longitude]}>
             <Popup>{farm.name}</Popup>
