51b1c90aa193dccc03816a0d34083f89cfb12cfe
Michael-Wilson88 <75252607+Michael-Wilson88@users.noreply.github.com>
2023-05-11 20:49:02 +0200

    Deleted /card package

--- Frontend/farmconnect/src/app/cards/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import FarmCardView from '@/components/card/FarmCardView';
-
-const KaartPagina: React.FC = () => {
-  return <div>{<FarmCardView />}</div>;
-};
-
-export default KaartPagina;
