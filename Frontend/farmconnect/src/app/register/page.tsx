052215da672bc61aa1c82fb281c0773be0194e05
BitsAndDroids <info@bitsanddroids.com>
2023-05-11 20:53:32 +0200

    Iets gedaan maar weet niet meer wat

--- Frontend/farmconnect/src/app/register/page.tsx
+++ Frontend/farmconnect/src/app/register/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 import {
   createUserWithEmailAndPassword,
+  getAdditionalUserInfo,
   GoogleAuthProvider,
   OAuthCredential,
   signInWithPopup,
+  UserCredential,
 } from '@firebase/auth';
 import { NextPage } from 'next';
 import React, { useState } from 'react';
 
 import { auth } from '@/firebase/config';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import googleLogo from '../../../public/logos/Google_-G-_Logo.svg.png';
 
 const RegisterPage: NextPage = () => {
   const [email, setEmail] = useState<string>(' ');
@@ -44,32 +48,34 @@ const RegisterPage: NextPage = () => {
     await createUserWithEmailAndPassword(auth, email, password).then(
       async (userCredential) => {
         // TODO dit moet naar de backend
-        const user = await userCredential.user;
-        const token: string = await userCredential.user.getIdToken();
-        const res = await fetch('http://localhost:3001/api/v1/users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: token,
-          },
-          body: JSON.stringify({
-            email_address: user.email,
-            user_name: user.email,
-            displayname: user.email,
-            firebaseId: user.uid,
-          }),
-        });
-        if (res.status === 200) {
-          console.log('User saved');
-        } else {
-          // If for some reason the user is created in firebase but not in the database, delete the user from firebase
-          console.log('User not saved');
-          await user.delete();
-        }
+        await registerToDatabse(userCredential);
       }
     );
   }
-
+  async function registerToDatabse(userCredential: UserCredential) {
+    const user = await userCredential.user;
+    const token: string = await userCredential.user.getIdToken();
+    const res = await fetch('http://localhost:3001/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        email_address: user.email,
+        user_name: user.email,
+        displayname: user.email,
+        firebaseId: user.uid,
+      }),
+    });
+    if (res.status === 200) {
+      console.log('User saved');
+    } else {
+      // If for some reason the user is created in firebase but not in the database, delete the user from firebase
+      console.log('User not saved');
+      await user.delete();
+    }
+  }
   /**
    * Register with Google
    */
@@ -78,9 +84,13 @@ const RegisterPage: NextPage = () => {
       .then(async (result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         console.log(credential);
+
         if (credential instanceof OAuthCredential) {
           const token = credential.accessToken;
         }
+        const aditionalUserInfo = getAdditionalUserInfo(result);
+        await registerToDatabse(result);
+        console.log(aditionalUserInfo);
         router.push('kaart');
         // The signed-in user info.
       })
@@ -124,10 +134,15 @@ const RegisterPage: NextPage = () => {
               onClick={() => registerWithGoogle()}
             >
               Register with Google{' '}
-              <img
-                src={'logos/Google_-G-_Logo.svg.png'}
+              <Image
+                src={googleLogo}
+                width={20}
+                height={20}
                 alt={'Google logo'}
                 className={'button__logo-provider'}
+                loading="eager"
+                priority={false}
+                
               />
             </button>
             <button className={'button--provider'} onClick={() => register()}>
