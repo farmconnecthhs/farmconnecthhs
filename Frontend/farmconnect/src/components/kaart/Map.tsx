9b0dc14f20f88a6d92a01b8db94b24a526cde37c
Dave <77780263+BitsAndDroids@users.noreply.github.com>
2023-05-13 15:23:08 +0200

    map aangepast

--- Frontend/farmconnect/src/components/kaart/Map.tsx
+++ Frontend/farmconnect/src/components/kaart/Map.tsx
@@ -49,8 +49,12 @@ const Map: React.FunctionComponent<MapProps> = ({ farms }) => {
       >
         <MapControler />
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=1bOawQCdnh61NAAZkKPUFGdbZbAUn4eAzQDI3kLZo3SuKCsq9vPMaKStpo7yT5if"
+          attribution='&copy; <a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          minZoom={0}
+          maxZoom={22}
+          subdomains="abcd"
+          accessToken="1bOawQCdnh61NAAZkKPUFGdbZbAUn4eAzQDI3kLZo3SuKCsq9vPMaKStpo7yT5if"
         />
         {farms.map((farm) => (
           <Marker key={farm.id} position={[farm.latitude, farm.longitude]}>
