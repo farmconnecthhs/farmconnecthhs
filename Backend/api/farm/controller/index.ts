9e018f795c3b86c3df689236cb57d7fd61a5289d
jdjan <20067879@student.hhs.nl>
2023-05-15 20:03:54 +0200

    Farm/FarmProfile controllers altered to provide json response

--- Backend/api/farm/controller/index.ts
+++ Backend/api/farm/controller/index.ts
@@ -1,11 +1,24 @@
+import { Request, Response } from 'express';
+
 import { Farm } from '../../../db/modules/farm/models/Farm';
 import * as FarmService from '../../../db/modules/farm/services/farmService';
 import { CreateFarmDTO } from '../dto/farm.dto';
 
-export const create = async (payload: CreateFarmDTO): Promise<Farm> => {
-  return await FarmService.create(payload);
+export const create = async (req: Request, res: Response) => {
+  try {
+    const payload: CreateFarmDTO = req.body;
+    const farm: Farm = await FarmService.create(payload);
+    res.status(201).json(farm);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
-export const getAll = async () => {
-  return await FarmService.getAll();
+export const getAll = async (req: Request, res: Response) => {
+  try {
+    const farms: Farm[] = await FarmService.getAll();
+    res.json(farms);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
--- Backend/api/farm_profile/controller/index.ts
+++ Backend/api/farm_profile/controller/index.ts
@@ -1,13 +1,24 @@
+import { Request, Response } from 'express';
+
 import { FarmProfile } from '../../../db/modules/farm_profile/models/FarmProfile';
 import * as FarmProfileService from '../../../db/modules/farm_profile/services/farmProfileService';
 import { CreateFarmProfileDTO } from '../dto/farmProfile.dto';
 
-export const create = async (
-  payload: CreateFarmProfileDTO
-): Promise<FarmProfile> => {
-  return await FarmProfileService.create(payload);
+export const create = async (req: Request, res: Response) => {
+  try {
+    const payload: CreateFarmProfileDTO = req.body;
+    const farmProfile: FarmProfile = await FarmProfileService.create(payload);
+    res.status(201).json(farmProfile);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
-export const getAll = async () => {
-  return await FarmProfileService.getAll();
+export const getAll = async (req: Request, res: Response) => {
+  try {
+    const farmProfiles: FarmProfile[] = await FarmProfileService.getAll();
+    res.json(farmProfiles);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
