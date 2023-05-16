cf94b97b682976e1037962fddadce1dc2d22c6ed
Dave <77780263+BitsAndDroids@users.noreply.github.com>
2023-05-16 14:48:28 +0200

    quick minor fix

--- Frontend/farmconnect/src/app/page.tsx
+++ Frontend/farmconnect/src/app/page.tsx
@@ -2,10 +2,19 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import styles from '@/app/kaart/Page.module.css';
+import FarmCardView from '@/components/card/FarmCardView';
 import MapComponent from '@/components/kaart/MapComponent';
 
 const KaartPagina: NextPage = () => {
-  return <div>{<MapComponent />}</div>;
+  return (
+    <div>
+      <MapComponent />
+      <div className={styles['card-container']}>
+        <FarmCardView />
+      </div>
+    </div>
+  );
 };
 
 export default KaartPagina;
