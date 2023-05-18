0bc136fbcad24756c0120ddc048156f3f16d5182
jdjan <20067879@student.hhs.nl>
2023-05-18 11:37:41 +0200

    Removed Create Farm(Profile) in controllers

--- Backend/api/farm/controller/index.ts
+++ Backend/api/farm/controller/index.ts
@@ -2,13 +2,6 @@ import { Request, Response } from 'express';
 
 import { Farm } from '../../../db/modules/farm/models/Farm';
 import * as FarmService from '../../../db/modules/farm/services/farmService';
-import { CreateFarmDTO } from '../dto/farm.dto';
-
-export const createFarm = async (req: Request, res: Response) => {
-  const payload: CreateFarmDTO = req.body;
-  const farm: Farm = await FarmService.create(payload);
-  return res.status(201).json(farm);
-};
 
 export const getAllFarms = async (req: Request, res: Response) => {
   const farms: Farm[] = await FarmService.getAll();
--- Backend/api/farm_profile/controller/index.ts
+++ Backend/api/farm_profile/controller/index.ts
@@ -2,13 +2,6 @@ import { Request, Response } from 'express';
 
 import { FarmProfile } from '../../../db/modules/farm_profile/models/FarmProfile';
 import * as FarmProfileService from '../../../db/modules/farm_profile/services/farmProfileService';
-import { CreateFarmProfileDTO } from '../dto/farmProfile.dto';
-
-export const createFarmProfile = async (req: Request, res: Response) => {
-  const payload: CreateFarmProfileDTO = req.body;
-  const farmProfile: FarmProfile = await FarmProfileService.create(payload);
-  return res.status(201).json(farmProfile);
-};
 
 export const getAllFarmProfiles = async (req: Request, res: Response) => {
   const farmProfiles: FarmProfile[] = await FarmProfileService.getAll();
