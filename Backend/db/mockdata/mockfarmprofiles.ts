4d9545ce9b2b738e6ee2b92b5168ace37965bbf3
jdjan <20067879@student.hhs.nl>
2023-05-18 17:51:19 +0200

    mockdatagenerator for farmprofiles

---
+++ Backend/db/mockdata/mockfarmprofiles.ts
@@ -0,0 +1,135 @@
+export const mockFarmProfiles = [
+  {
+    address: 'Draakstraat 1',
+    postalCode: '1000AA',
+    city: 'Amsterdam',
+    latitude: 52.370216,
+    longitude: 4.895168,
+    farmDescription:
+      'Prachtige boerderij bekend als De groene draak. Bekend om onze duurzame landbouwpraktijken.',
+    productDescription: 'Wij produceren de beste melk en kaas in de regio.',
+    email: 'info@groenedraak.nl',
+    website: 'www.groenedraak.nl',
+    phone: '+31123456789',
+    farmId: 1,
+  },
+  {
+    address: 'Paardstraat 2',
+    postalCode: '2000BB',
+    city: 'Rotterdam',
+    latitude: 51.9225,
+    longitude: 4.47917,
+    farmDescription:
+      'Bij Het gulden paard focussen we op de productie van biologisch vlees en eieren.',
+    productDescription:
+      'Hoogwaardig biologisch rundvlees en verse eieren direct van de boerderij.',
+    email: 'contact@guldenpaard.nl',
+    website: 'www.guldenpaard.nl',
+    phone: '+31123456789',
+    farmId: 2,
+  },
+  {
+    address: 'Zwaanstraat 3',
+    postalCode: '3000CC',
+    city: 'Utrecht',
+    latitude: 52.0894444,
+    longitude: 5.1077981,
+    farmDescription:
+      'De witte zwaan is een kleine familieboerderij met een focus op duurzaamheid.',
+    productDescription:
+      'Wij leveren verse groenten en fruit van hoge kwaliteit, rechtstreeks van onze boerderij.',
+    email: 'informatie@wittezwaan.nl',
+    website: 'www.wittezwaan.nl',
+    phone: '+31123456789',
+    farmId: 3,
+  },
+  {
+    address: 'Schaapstraat 4',
+    postalCode: '4000DD',
+    city: 'Den Haag',
+    latitude: 52.078663,
+    longitude: 4.288788,
+    farmDescription:
+      'Het zwarte schaap is gespecialiseerd in het produceren van biologische wol.',
+    productDescription: 'Wij bieden premium kwaliteit wol en wollen producten.',
+    email: 'klantenservice@zwarteschaap.nl',
+    website: 'www.zwarteschaap.nl',
+    phone: '+31123456789',
+    farmId: 4,
+  },
+  {
+    address: 'Vlinderstraat 5',
+    postalCode: '5000EE',
+    city: 'Eindhoven',
+    latitude: 51.441642,
+    longitude: 5.469722,
+    farmDescription:
+      'Bij de Blauwe vlinder boerderij houden we ons bezig met de productie van honing en bijenwasproducten.',
+    productDescription:
+      'Heerlijke honing en hoogwaardige bijenwasproducten direct van de boerderij.',
+    email: 'contact@blauwevlinder.com',
+    website: 'www.blauwevlinder.com',
+    phone: '+31123456789',
+    farmId: 5,
+  },
+  {
+    address: 'Olifantstraat 6',
+    postalCode: '6000FF',
+    city: 'Tilburg',
+    latitude: 51.560596,
+    longitude: 5.091914,
+    farmDescription:
+      'De roze olifant is gespecialiseerd in de productie van ambachtelijke zuivelproducten.',
+    productDescription:
+      'Wij bieden een breed assortiment aan zuivelproducten zoals melk, kaas en yoghurt.',
+    email: 'contact@derozeolifant.com',
+    website: 'www.derozeolifant.com',
+    phone: '+31123456789',
+    farmId: 6,
+  },
+  {
+    address: 'Koestraat 7',
+    postalCode: '7000GG',
+    city: 'Groningen',
+    latitude: 53.219383,
+    longitude: 6.566502,
+    farmDescription:
+      'De rode koe is een biologische boerderij die bekend staat om haar premium rundvlees.',
+    productDescription:
+      'Wij leveren biologisch rundvlees direct van de boerderij naar uw keukentafel.',
+    email: 'contact@derodekoe.com',
+    website: 'www.derrodekoe.com',
+    phone: '+31123456789',
+    farmId: 7,
+  },
+  {
+    address: 'Kipstraat 8',
+    postalCode: '8000HH',
+    city: 'Nijmegen',
+    latitude: 51.812563,
+    longitude: 5.837226,
+    farmDescription:
+      'Bij De gele kip richten we ons op de productie van scharrelkippen en eieren.',
+    productDescription:
+      'Gezonde, blije kippen leveren de beste eieren en het lekkerste vlees.',
+    email: 'contact@degelekip.com',
+    website: 'www.degelekip.com',
+    phone: '+31123456789',
+    farmId: 8,
+  },
+  {
+    address: 'Krokodilstraat 9',
+    postalCode: '9000II',
+    city: 'Haarlem',
+    latitude: 52.387388,
+    longitude: 4.646219,
+    farmDescription:
+      'De paarse krokodil is een boerderij die zich toelegt op de teelt van biologische groenten en fruit.',
+    productDescription:
+      'Wij bieden een verscheidenheid aan biologische seizoensproducten.',
+    email: 'contact@depaarsekrokodil.com',
+    website: 'www.depaarsekrokodil.com',
+    phone: '+31123456789',
+    farmId: 9,
+  },
+];
