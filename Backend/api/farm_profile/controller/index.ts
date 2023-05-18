4f9ba4aa9c1ecfb9e08dc64a89583fe16835c9bc
jdjan <20067879@student.hhs.nl>
2023-05-18 11:23:19 +0200

    Removed Status 4xx status codes from controllers.

--- Backend/api/farm_profile/controller/index.ts
+++ Backend/api/farm_profile/controller/index.ts
@@ -6,27 +6,11 @@ import { CreateFarmProfileDTO } from '../dto/farmProfile.dto';
 
 export const createFarmProfile = async (req: Request, res: Response) => {
   const payload: CreateFarmProfileDTO = req.body;
-  if (!payload) {
-    return res
-      .status(400)
-      .json({ error: 'Bad Request: Invalid Farm Profile Data' });
-  }
-
   const farmProfile: FarmProfile = await FarmProfileService.create(payload);
-  if (!farmProfile) {
-    return res
-      .status(400)
-      .json({ error: 'Bad Request: Farm Profile could not be created' });
-  }
-
   return res.status(201).json(farmProfile);
 };
 
 export const getAllFarmProfiles = async (req: Request, res: Response) => {
   const farmProfiles: FarmProfile[] = await FarmProfileService.getAll();
-  if (farmProfiles.length === 0) {
-    return res.status(404).json({ error: 'Not Found: No Farm Profiles' });
-  }
-
   return res.status(200).json(farmProfiles);
 };
