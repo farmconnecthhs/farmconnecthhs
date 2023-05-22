a2d417794917001ed3b0acefc97224db69326297
Quinten <quintenvollmer@live.nl>
2023-05-22 20:10:18 +0200

    somehow I created a loop which spams the server...

--- Frontend/farmconnect/src/app/gebruiker/[id]/page.tsx
+++ Frontend/farmconnect/src/app/gebruiker/[id]/page.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { NextPage } from 'next';
+import { useSearchParams } from 'next/navigation';
 
 import type { User } from '@/components/gebruiker/profile/interfaces/User';
 
@@ -6,17 +8,35 @@ type ProfileProps = {
   user: User;
 };
 
-const ProfielPage: NextPage<ProfileProps> = ({ user }) => {
+// prettier-ignore
+{/* @ts-expect-error Async Server Component */}
+const ProfielPage: NextPage<ProfileProps> = async () => {
+  const searchParams = useSearchParams();
+  const id: string | null = searchParams.get('id');
+  console.log(id);
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:3001/api/v1/users/firebase/' + id
+      );
+      return await response.json();
+    } catch (error) {
+      console.log('Error fetching data:', error);
+      return [];
+    }
+  };
+
+  const userData = await fetchUserData();
+
   return (
     <div className={'content-container'}>
-      <h2>{user.gebruikersnaam}</h2>
+      <h2>{userData.gebruikersnaam}</h2>
       <p>
-        Naam: {user.voornaam} {user.achternaam}
+        Naam: {userData.voornaam} {userData.achternaam}
       </p>
-      <p>Email: {user.email}</p>
-      <p>Telefoonnummer: {user.telefoonnummer}</p>
+      <p>Email: {userData.email}</p>
+      <p>Telefoonnummer: {userData.telefoonnummer}</p>
     </div>
   );
 };
-
 export default ProfielPage;
