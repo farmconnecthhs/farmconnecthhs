cfb4b12c6960d539cf4702aeab00846de6faae48
Michael-Wilson88 <75252607+Michael-Wilson88@users.noreply.github.com>
2023-05-29 20:00:59 +0200

    Connected the Openingstijden with the Business hours in the backend

--- Frontend/farmconnect/src/components/boerderij/profile/OpeningstijdenViewRow.tsx
+++ Frontend/farmconnect/src/components/boerderij/profile/OpeningstijdenViewRow.tsx
@@ -1,10 +1,8 @@
-
 import React from 'react';
 
-import styles from './OpeningstijdenViewRow.module.css'
+import styles from './OpeningstijdenViewRow.module.css';
 
 import { Openingstijd } from '@/components/boerderij/editor/interfaces/Openingstijd';
-
 
 interface OpeningstijdenViewRowProps {
   openingstijd: Openingstijd;
@@ -12,28 +10,45 @@ interface OpeningstijdenViewRowProps {
 
 const OpeningstijdenViewRow: React.FunctionComponent<
   OpeningstijdenViewRowProps
-  > = (props: OpeningstijdenViewRowProps) => {
+> = (props: OpeningstijdenViewRowProps) => {
   const { openingstijd } = props;
 
   const formatTime = (time: Date | string | undefined) => {
     if (time instanceof Date) {
-      return time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      return time.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
     } else if (typeof time === 'string') {
       const date = new Date(time);
-      return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      return date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
     }
     return '';
   };
 
   const getDayAbbreviation = (day: number) => {
-    const days = ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'];
+    const days = [
+      'Zondag',
+      'Maandag',
+      'Dinsdag',
+      'Woensdag',
+      'Donderdag',
+      'Vrijdag',
+      'Zaterdag',
+    ];
     return days[day];
   };
 
   return (
     <div className={styles.row}>
       <span className={styles.day}>{getDayAbbreviation(openingstijd.day)}</span>
-      <span className={styles.time}>{formatTime(openingstijd.openTime)} - {formatTime(openingstijd.closeTime)}</span>
+      <span className={styles.time}>
+        {formatTime(openingstijd.openTime)} -{' '}
+        {formatTime(openingstijd.closeTime)}
+      </span>
     </div>
   );
 };
